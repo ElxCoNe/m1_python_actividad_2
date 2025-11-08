b31c713973e2c602414a31a2c54e2d230443afaa
Juan C Hernandez <125905912+ElxCoNe@users.noreply.github.com>
2025-11-07 21:25:38 -0500

    lo actualice

--- ejercicio_2.py
+++ ejercicio_2.py
@@ -1,19 +1,18 @@
-print("1.Crear un nuevo animal \n2.Listar todos los animales \n3.Borrar un animal por ID(posicion \n4.Borrar un animal por nombre \n5.Borrar todos los animales \n6. Listar un animal por ID(posicion \n7.Salir")
-
-
 animals = []
 
 while True:
+
+    print("1.Crear un nuevo animal \n2.Listar todos los animales \n3.Borrar un animal por ID(posicion \n4.Borrar un animal por nombre \n5.Borrar todos los animales \n6. Listar un animal por ID(posicion \n7.Salir")
+    
     option = int(input("Por favor selecciona la opción que deseas: "))
 
     match option:
-        case 1:
 
+        case 1:
             animal = input("Que animal deseas agregar: ")
             animals.append(animal)
         
         case 2:
-            
             if animals:
                 print("Estos son los animales que tienes al momento..")
                 for i, animal in enumerate(animals):
@@ -22,14 +21,11 @@ while True:
                 print("No hay animales en la lista hasta el momento, por favor agregar alguno")
 
         case 3:
-
             idDelete = int(input("Por favor ingresar el ID del animal que deseas eliminar: "))
-
             delete = animals.pop(idDelete)
             print(f"Haz eliminado {delete} de la lista con exito")
 
         case 4:
-
             deleteName = input("Por favor ingresar el nombre del animal que deseas eliminar: ")
             animals.remove(deleteName)
 
@@ -39,7 +35,6 @@ while True:
 
         case 6:
             animalPrint = int(input("Por favor ingresar el id del animal que deseas listar"))
-
             print(f"El animal es: {animals[animalPrint]}")
         
         case 7:
@@ -47,7 +42,6 @@ while True:
 
         case _:
             print("La opción seleccionada no existe, por favor volver a intentarlo")
-
 
 
 
